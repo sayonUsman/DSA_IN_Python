b5ba9cf2e05274b6a131da12939c59fea7d23f60
Sayon <sayonusman.ma@gmail.com>
2022-03-28 05:52:09 +0600

    Quick Sort Algorithm2 is added

---
+++ quick_sort2.py
@@ -0,0 +1,34 @@
+class QuickSort:
+
+    def quick_sort(self, __list__):
+
+        if len(__list__) < 2:
+            return __list__
+
+        else:
+            mid_index = len(__list__) // 2
+            pivot = __list__[mid_index]
+            __list__.remove(__list__[mid_index])
+            greater_than_pivot = []
+            less_than_pivot = []
+
+            for item in __list__:
+
+                if item > pivot:
+                    greater_than_pivot.append(item)
+
+                else:
+                    less_than_pivot.append(item)
+
+            return self.quick_sort(less_than_pivot) + [pivot] + self.quick_sort(greater_than_pivot)
+
+
+try:
+    quickSort = QuickSort()
+    numbers = list(
+        map(int, input('Please enter the list of number:\n').split()))
+    numbers = quickSort.quick_sort(numbers)
+    print(numbers)
+
+except Exception as error:
+    print(error)
